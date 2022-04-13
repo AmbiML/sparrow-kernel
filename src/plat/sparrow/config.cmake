d247b7a5feddd1d75e712bf2f9594027e340519d
Adam Jesionowski <jesionowski@google.com>
2022-04-13 18:21:17 +0000

    Merge "Add Kata SW timer to device tree."
    GitOrigin-RevId: abd8ee2d548c49863a3fcc6985495d6dd454d311

--- src/plat/sparrow/config.cmake
+++ src/plat/sparrow/config.cmake
@@ -19,7 +19,7 @@ if(KernelPlatformSparrow)
         TIMER_FREQUENCY 24000000
 
         INTERRUPT_CONTROLLER drivers/irq/smc_plic.h
-        PLIC_MAX_NUM_INT 14
+        PLIC_MAX_NUM_INT 15
     )
 else()
     unset(KernelPlatformFirstHartID CACHE)
